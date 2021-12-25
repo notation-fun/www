e0ccd7aa795fd7c92e7676dab8a19f03b2de56a6
YJ Park <yjpark@gmail.com>
2021-12-25 14:50:11 +0800

    2021-12-25 14:50:11

--- static/notation/wasm.js
+++ static/notation/wasm.js
@@ -259,7 +259,7 @@ function addBorrowedObject(obj) {
 }
 function __wbg_adapter_58(arg0, arg1, arg2) {
     try {
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdce6af9be987dcf7(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc8aed37fb457801c(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
@@ -1229,7 +1229,7 @@ async function init(input) {
         var ret = makeMutClosure(arg0, arg1, 11246, __wbg_adapter_58);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper29907 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper29906 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 13614, __wbg_adapter_61);
         return addHeapObject(ret);
     };
